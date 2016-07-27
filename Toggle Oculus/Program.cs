64e1d562f0d555f77ceda9b6c3ed26d9e9204a25
Yaroslav Shirokov <slava@github.com>
2016-07-26 22:44:54 -0700

    The smallest thing that could possibly work

--- Toggle Oculus/Program.cs
+++ Toggle Oculus/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ServiceProcess;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace Toggle_Oculus
 {
@@ -10,6 +10,27 @@ namespace Toggle_Oculus
     {
         static void Main(string[] args)
         {
+            // From https://msdn.microsoft.com/en-us/library/system.serviceprocess.servicecontroller.stop(v=vs.110).aspx
+            // Because computers are hard, but I'm very angry
+
+            // Toggle the Oculus service - 
+            // If it is started (running, paused, etc), stop the service.
+            // If it is stopped, start the service.
+            ServiceController sc = new ServiceController("OVRService");
+
+            if ((sc.Status.Equals(ServiceControllerStatus.Stopped)) ||
+                 (sc.Status.Equals(ServiceControllerStatus.StopPending)))
+            {
+                sc.Start();
+            }
+            else
+            {
+                sc.Stop();
+            }
+
+            // Refresh and display the current service status.
+            sc.Refresh();
+            MessageBox.Show($"The Oculus service status is now set to {sc.Status.ToString()}.");
         }
     }
 }
